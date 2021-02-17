15f8a548de1486a51da76f885051e99a8391ff29
Key Admin <mdepeiza@keyconceptskb.com>
2021-02-17 12:29:42 -0500

    Added DbSet<Superhero> to ApplicationDbContext.

--- SuperheroCreator/Data/ApplicationDbContext.cs
+++ SuperheroCreator/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperheroCreator.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,7 @@ namespace SuperheroCreator.Data
             : base(options)
         {
         }
+
+        public DbSet<Superhero> Superheroes { get; set; }
     }
 }
