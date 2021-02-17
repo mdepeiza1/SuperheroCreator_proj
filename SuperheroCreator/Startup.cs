2aa25825329c2be26d8e70261975e3698afb0c0c
Key Admin <mdepeiza@keyconceptskb.com>
2021-02-17 15:21:29 -0500

    Changed home page to superhero index.

--- SuperheroCreator/Startup.cs
+++ SuperheroCreator/Startup.cs
@@ -62,7 +62,7 @@ namespace SuperheroCreator
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Superhero}/{action=Index}/{id?}"); //pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
